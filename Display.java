bb08227c90e019d8c1dd83bad47700447b29c706
AvilMP <adrek80@wp.pl>
2022-11-13 13:04:42 +0100

    v0,2

--- Display.java
+++ Display.java
@@ -3,11 +3,21 @@ public class Display {
     
     public String text(int index){
         switch(index){
-            case 1: return "Javacus v 1.0";
+            case 1: return "JTC v 1.0";
             case 2: return "Wybierz operacje: ";
             case 3: return "Wprowadz wartość: ";
             case 4: return "Wynik: ";
-            default: return "null";
+            case 5: return " + ";
+            case 6: return " - ";
+            //case 7: return " * ";
+            //case 8: return " / ";
+            case 9: return "Q. +.";
+            case 10: return "W. -.";
+            case 11: return "X. Wynik.";
+            case 12: return "V. Wyjście.";
+            case 13: return " ";
+            case 14: return ": ";
+            default: return "_";
         }
     }
 }
--- Mathops.java
+++ Mathops.java
@@ -1,10 +1,12 @@
 
 public class Mathops {
     //user cout
-    float[] intigers = new float[20];
+    float[] intigers = new float[2];
     char[]  operand = new char[18];
-    //basic op
+    double result;
+
+    //typical op
     Add addition;
     Sub subtraction;
-    //adwanced op
+    //specjal op
 }
