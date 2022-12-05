b5affb2cb022448824d560273b4264e01910e13c
AvilMP <adrek80@wp.pl>
2022-12-05 12:32:16 +0100

    v0.4.0

--- Master.java
+++ Master.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class Master {
-    
+
     Mathops mathops = new Mathops();
     Scanner sc = new Scanner(System.in);
 
-    private String appName = new String("Jtc v 0.3.5");
+    private String appName = new String("Jtc v0.4.0");
     private String stdMsg = new String("=. Wynik.\nc. Reset\ne. Wyjście\n>>>");
 
     public String text(int index){
@@ -21,33 +21,49 @@ public class Master {
         }
     }
 
-    void outputMenager(){
+    public void outputMenager(){
         for(int i = 1; i <= 4; i++){
-            if(i == 2){    //print options
-                for(int j = 7; j <= 7 + (mathops.modules); j++){
-                    System.out.println(text(j));
-                }
-            }
-            else if(i == 3){     //print numbs & ops
-                for(int f = 0; f < mathops.wideTable; f++){
-                    if(mathops.getNumb(f) == 0){
-                        break;
-                    }
-                    else{
-                        System.out.print(mathops.getNumb(f)); System.out.print(' ');
-                    }
-                    System.out.print(mathops.getChar(f)); System.out.print(' '); 
-                }
+            switch(i){
+                case 1: System.out.println(text(i));
+                    break;  //print title.
+                case 2:
+                        for(int j = 7; j <= 7 + mathops.modules; j++){
+                            System.out.println(text(j));
+                        }
+                        System.out.println(text(i));
+                    break;  //print options.
+                case 3: for(int f = 0; f < mathops.wideTable; f++){
+                            if(mathops.getNumb(f) == 0){
+                                break;
+                            }
+                            else{
+                                System.out.print(mathops.getNumb(f)); System.out.print(' ');
+                            }
+                            System.out.print(mathops.getChar(f)); System.out.print(' '); 
+                        }
+                        System.out.println();
+                        System.out.println(text(i) + mathops.getResult());
+                    break;  //print numb + op.
+                case 4: System.out.println(text(i));
+
+                //test*
                 System.out.println();
+                    System.out.println("SP = " + mathops.stackPointer());
+                //*test
+                    break;  //print input op.
             }
-            else if(i == 4){
-                //...
-            }
-            System.out.println(text(i));
         }
     }
 
-    boolean exit(){
+    public void inputMenager(int select){ //select from stackPointer();
+        //...
+    }
+
+    public void reset(){
+        //...
+    }
+
+    public boolean exit(){
         // ...
         return false;
     }
--- Mathops.java
+++ Mathops.java
@@ -1,21 +1,36 @@
 
 public class Mathops {
     public static byte wideTable = 30;  //max user numbers input.
-    private float[] intigers = new float[wideTable];
+    private double[] intigers = new double[wideTable];
     private char[]  operand = new char[wideTable];
     private double result;
-    public byte modules;
+    public byte modules;    //(mod<<)  add,sub...etc...
 
     public Mathops(){
         for(int i = 0; i < wideTable; i++){
             this.intigers[i] = 0;
+            //t*
+            //this.intigers[0] =1.3;
+            //this.intigers[1] =2.3;
+            //*t
             this.operand[i]  = 0;
         }
         this.result = 0;
         this.modules = (2) - 1; // (mod/mds).
     }
 
-    float getNumb(int tabNumb){
+    public int stackPointer(){
+        int i = -1;
+        if(this.intigers[29] != 0){
+            return 0;
+        }
+        do{
+            i++;
+        }while(this.intigers[i] != 0);
+            return i;
+    } 
+
+    double getNumb(int tabNumb){
         return intigers[tabNumb];
     }
 
@@ -23,13 +38,16 @@ public class Mathops {
         return operand[tabChar];
     }
 
-    //typical op
-    Add addition = new Add();
-
-    public void add(){
-        //
+    double getResult(){
+        return this.result;
     }
 
-    Sub subtraction = new Sub();
-    //specjal op
+    //
+    Add addition = new Add(); // mod++
+
+    public void add(){
+        //...
+    }
+
+    Sub subtraction = new Sub(); //mod++
 }
